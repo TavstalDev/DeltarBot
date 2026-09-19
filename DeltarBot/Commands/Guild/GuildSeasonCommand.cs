515be4d630080c49e40e4b52a334304d19afb72e
tavstal <tavstal@proton.me>
2026-09-19 15:48:47 +0200

    feat: rename GuildStatsCommand to GuildSeasonCommand and add guild search options

---
+++ DeltarBot/Commands/Guild/GuildSeasonCommand.cs
@@ -0,0 +1,92 @@
+using Discord;
+using Discord.WebSocket;
+using Tavstal.DeltarBot.Models.Commands;
+using Tavstal.DeltarBot.Services;
+using Tavstal.WynnNetSDK.Exceptions;
+using Tavstal.WynnNetSDK.Http;
+
+namespace Tavstal.DeltarBot.Commands.Guild;
+
+public class GuildSeasonCommand : SimpleCommand
+{
+    public GuildSeasonCommand(DiscordSocketClient client, WynnHttpClient wynnClient, GuildService guildService) : base(
+        "gseason",
+        "Gets seasonal information about the provided guild.",
+        client, wynnClient, guildService) { }
+
+    public override SlashCommandBuilder HandleBuild(SlashCommandBuilder cmd)
+    {
+        SlashCommandOptionBuilder opt = new SlashCommandOptionBuilder()
+            .WithName("guild")
+            .WithDescription("The name of the guild you want to search for.")
+            .WithRequired(true)
+            .WithType(ApplicationCommandOptionType.String);
+        cmd.AddOption(opt);
+        opt = new SlashCommandOptionBuilder()
+            .WithName("isprefix")
+            .WithDescription("Whether it should be the prefix of the guild you want to search for.")
+            .WithRequired(false)
+            .WithType(ApplicationCommandOptionType.Boolean);
+        cmd.AddOption(opt);
+        return cmd;
+    }
+    
+    public override async Task HandleAsync(SocketSlashCommand data)
+    {
+        if (data.Data.Options.Count == 0)
+        {
+            await data.RespondAsync("You must provide arguments.", ephemeral: true);
+            return;
+        }
+        
+        var guildOption = data.Data.Options.ElementAt(0);
+        var guildName = (string)guildOption.Value;
+        
+        bool isPrefix = false;
+        if (data.Data.Options.Count == 2)
+        {
+            var prefixOption = data.Data.Options.ElementAt(1);
+            isPrefix = (bool)prefixOption.Value;
+        }
+
+        if (isPrefix && guildName.Length > 4)
+        {
+            await data.RespondAsync("The prefix name cannot be longer than 4 characters.", ephemeral: true);
+            return;
+        }
+        
+        try
+        {
+            var result = isPrefix
+                ? await _wynnClient.Guild.GetByPrefixAsync(guildName)
+                : await _wynnClient.Guild.GetByNameAsync(guildName);
+
+            if (result.IsError)
+            {
+                await data.RespondAsync(result.Error?.Message ?? "Failed to get guilds.", ephemeral: true);
+                return;
+            }
+            
+            var guild = result.Value;
+            var embed = new EmbedBuilder()
+                .WithTitle($"[{guild.Prefix}] {guild.Name}")
+                .WithThumbnailUrl("https://raw.githubusercontent.com/TavstalDev/DeltarBot/refs/heads/master/assets/images/icon_guild.png")
+                .WithColor(Color.Blue)
+                .WithTimestamp(guild.Created);
+
+            string description = string.Empty;
+            foreach (var season in guild.SeasonRanks.OrderBy(x => x.Key))
+            {
+                description += $"Season {season.Key} • Rating: {season.Value.Rating} • Territories: {season.Value.FinalTerritories} \n";
+            }
+
+            embed.WithDescription(description);
+            
+            await data.RespondAsync(embed: embed.Build());
+        }
+        catch (RateLimitException ex)
+        {
+            await data.RespondAsync(ex.Message, ephemeral: true);
+        }
+    }
+}
