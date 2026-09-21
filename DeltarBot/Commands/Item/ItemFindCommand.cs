da380c3899680df7fa0b51ca06708bb3b2426e8f
Tavstal <tavstal@gmail.com>
2026-09-21 14:59:38 +0200

    feat: implement item find command with detailed item search and response embed

--- DeltarBot/Commands/Item/ItemFindCommand.cs
+++ DeltarBot/Commands/Item/ItemFindCommand.cs
@@ -1,8 +1,13 @@
+using System.Text;
+using System.Text.RegularExpressions;
 using Discord;
 using Discord.WebSocket;
 using Tavstal.DeltarBot.Models.Commands;
 using Tavstal.DeltarBot.Services;
+using Tavstal.WynnNetSDK.Exceptions;
 using Tavstal.WynnNetSDK.Http;
+using Tavstal.WynnNetSDK.Http.Requests.Items.Bodies;
+using Tavstal.WynnNetSDK.Models.Leaderboard;
 
 namespace Tavstal.DeltarBot.Commands.Item;
 
@@ -10,16 +15,67 @@ public class ItemFindCommand : SimpleCommand
 {
     public ItemFindCommand(DiscordSocketClient client, WynnHttpClient wynnClient, GuildService guildService) : base(
         "ifind",
-        "TODO",
+        "Searches for items with matching name",
         client, wynnClient, guildService) { }
 
     public override SlashCommandBuilder HandleBuild(SlashCommandBuilder cmd)
     {
+        SlashCommandOptionBuilder opt = new SlashCommandOptionBuilder()
+            .WithName("name")
+            .WithDescription("Name of the item to find.")
+            .WithRequired(true)
+            .WithType(ApplicationCommandOptionType.String);
+        cmd.AddOption(opt);
         return cmd;
     }
     
     public override async Task HandleAsync(SocketSlashCommand data)
     {
+        if (data.Data.Options.Count == 0)
+        {
+            await data.RespondAsync("You must provide arguments.", ephemeral: true);
+            return;
+        }
         
+        var itemOption = data.Data.Options.ElementAt(0);
+        var itemName = (string)itemOption.Value;
+
+        try
+        {
+            var result = await _wynnClient.Items.SearchAsync(new ItemSearchRequestBody
+            {
+                Query = itemName
+            });
+
+            if (result.IsError)
+            {
+                await data.RespondAsync(result.Error?.Message ?? "Failed to get item data.", ephemeral: true);
+                return;
+            }
+            
+            var itemResult = result.Value;
+            var embed = new EmbedBuilder()
+                .WithTitle($"Found {itemResult.Results.Count} items.")
+                .WithUrl("https://github.com/TavstalDev/DeltarBot")
+                .WithColor(Color.Blue)
+                .WithCurrentTimestamp()
+                .WithFooter("DeltarBot");
+
+            var description = new StringBuilder();
+            foreach (var item in itemResult.Results)
+            {
+                description.AppendLine($"**{item.DisplayName}**");
+                description.AppendLine($"> **Type:** {item.Type}");
+                description.AppendLine($"> **SubType:** {item.SubType}");
+                description.AppendLine($"> **Tier:** {item.Tier}");
+            }
+            embed.WithDescription(description.ToString());
+            
+            await data.RespondAsync(embed: embed.Build());
+        }
+        catch (RateLimitException ex)
+        {
+            await data.RespondAsync(ex.Message, ephemeral: true);
+        }
     }
 }
